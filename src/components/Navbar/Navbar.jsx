c2b2e824e62af356fd99fc39ce5c02ba3407304d
hackelmo <tokkito88@gmail.com>
2023-02-19 21:45:04 +0900

    🔧 Add react-icons

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <header>
+      <div></div>
+      <form>
+        <input type="text" />
+        <button>검색</button>
+      </form>
+    </header>
+  );
+}
